ce588db3e946240af0d4581de739491e6969bbcb
fabiolaegarcial <67425045+fabiolaegarcial@users.noreply.github.com>
2022-07-28 19:39:50 -0500

    Finished home page (mobile) content. No footer here. Navbar needs to be changed.

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
+// Blog Map
 function myMap() {
     var mapProp= {
       center:new google.maps.LatLng(33.7748,-84.2963), //Decatur, GA
       zoom:12,//higher # greater zoom
+      styles: stylesArray
     };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    }
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+    var marker = new google.maps.Marker({position: myCenter});
+    marker.setMap(mapProp);
+}
+var stylesArray = [
+    {
+        "featureType": "all",
+        "stylers": [
+        ]
+      },{
+        "featureType": "poi.park",
+        "elementType": "labels.text.stroke", //stroke - fill
+        "stylers": [
+          { "color": "#000000" }
+        ]
+      },{
+        "featureType": "landscape",
+        "elementType": "labels",
+        "stylers": [
+          { "visibility": "off" }
+        ]
+      }
+  ]
